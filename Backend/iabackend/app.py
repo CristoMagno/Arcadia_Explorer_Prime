3992b958a3bc1545f65fe493bc2d6bfec6ec0792
Cristo Magno <isc211937@itsatlixco.edu.mx>
2025-05-27 05:57:59 -0600

    Modificar app.py para producción y agregar Dockerfile

--- Backend/iabackend/app.py
+++ Backend/iabackend/app.py
@@ -1,5 +1,4 @@
-# Arcadia/iabackend/app.py
-
+import os
 from flask import Flask
 from flask_cors import CORS
 from graph_utils import cargar_grafo_vial, cargar_grafo_transporte
@@ -7,11 +6,8 @@ from rutas import rutas
 from optimal_route import blueprint as optimal_route_blueprint
 
 app = Flask(__name__)
-#CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
 CORS(app, resources={r"/api/*": {"origins": ["http://localhost:3000", "https://miapp.vercel.app"]}})
 
-
-# Cargar el grafo vial y red de transporte desde funciones auxiliares
 print("Cargando red vial...")
 G_vial = cargar_grafo_vial()
 print("Red vial cargada.")
@@ -20,8 +16,9 @@ print("Cargando red de transporte...")
 G_transporte, stops_info = cargar_grafo_transporte()
 print("Red de transporte cargada.")
 
-# Registrar el blueprint del endpoint /api/optimal-route
 app.register_blueprint(optimal_route_blueprint(G_vial, G_transporte, stops_info, rutas))
 
+# ❌ NO pongas app.run() aquí, para producción con Gunicorn
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
